5e04109985d7607d7d60c1c9b59489abc9cda966
NagendraChaudhary <nagnedrachaudhary2016@gmail.com>
2022-04-20 13:36:08 +0530

    s3 added

--- app/src/main/java/com/onebanc/obcb/DocumentListFragment.java
+++ app/src/main/java/com/onebanc/obcb/DocumentListFragment.java
@@ -2,41 +2,34 @@ package com.onebanc.obcb;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DocumentListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Objects;
+
 public class DocumentListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button btnReady;
+    private ImageButton ivClose;
 
     public DocumentListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DocumentListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static DocumentListFragment newInstance(String param1, String param2) {
         DocumentListFragment fragment = new DocumentListFragment();
         Bundle args = new Bundle();
@@ -58,7 +51,20 @@ public class DocumentListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_document_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnReady = view.findViewById(R.id.btn_ready);
+        ivClose = view.findViewById(R.id.ib_cross);
+        btnReady.setOnClickListener(view1 -> {
+            Navigation.findNavController(view).navigate(R.id.action_documentListFragment_to_permissionListFragment);
+        });
+
+        ivClose.setOnClickListener(view2 -> {
+            Objects.requireNonNull(getActivity()).onBackPressed();
+        });
     }
 }
