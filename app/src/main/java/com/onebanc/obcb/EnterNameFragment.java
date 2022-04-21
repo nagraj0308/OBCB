5349b0ad6006f6f2cd41aa9fe27f446abc4cd0e5
NagendraChaudhary <nagnedrachaudhary2016@gmail.com>
2022-04-21 19:01:23 +0530

    s4 added

--- app/src/main/java/com/onebanc/obcb/EnterNameFragment.java
+++ app/src/main/java/com/onebanc/obcb/EnterNameFragment.java
@@ -37,15 +37,7 @@ public class EnterNameFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EnterNameFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static EnterNameFragment newInstance(String param1, String param2) {
         EnterNameFragment fragment = new EnterNameFragment();
         Bundle args = new Bundle();
